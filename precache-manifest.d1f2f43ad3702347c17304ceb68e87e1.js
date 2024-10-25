f9f8f52f506f39403defc007ff580beecbb84b61
Korsawat Baiseejuang <korsawat.bai@student.mahidol.edu>
2024-10-25 17:29:51 +0700

    Updates

---
+++ precache-manifest.d1f2f43ad3702347c17304ceb68e87e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f1b2756894f000b68fbdfb91bc11c2",
+    "url": "/Maskridle/index.html"
+  },
+  {
+    "revision": "d8f44a0e993a53869a03",
+    "url": "/Maskridle/static/css/main.97e22dfa.chunk.css"
+  },
+  {
+    "revision": "6ea791521c58bb6f51a3",
+    "url": "/Maskridle/static/js/2.e1a6285c.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/Maskridle/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "d8f44a0e993a53869a03",
+    "url": "/Maskridle/static/js/main.069f3456.chunk.js"
+  },
+  {
+    "revision": "74b4b32b160a4d1d3b5c",
+    "url": "/Maskridle/static/js/runtime~main.697762b8.js"
+  },
+  {
+    "revision": "48807c4a4bb460789c01d517edb370ab",
+    "url": "/Maskridle/static/media/maskrider.48807c4a.csv"
+  }
+]);
